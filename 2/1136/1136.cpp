838842e9c44c3d070902fe1b00a9377eb106c565
liymouse <liymouse@gmail.com>
2022-01-04 11:28:17 +0800

    add 1136 1137

---
+++ 2/1136/1136.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+#define PI (3.14159265358979)
+
+int main()
+{
+    int n;
+    double r;
+    int ca = 1;
+    while (scanf("%d %lf", &n, &r) == 2)
+    {
+        if (n == 0 && r == 0) break;
+        int px[500], py[500];
+        double res = 0.0;
+        for (int i = 0; i < n; i++) scanf("%d %d", &px[i], &py[i]);
+        int curx = 0, cury = 0;
+        double dx = -1, dy = 0;
+        while (1)
+        {
+            double L = -1;
+            double r2 = r * r;
+            int nxti = -1;
+            for (int i = 0; i < n; i++)
+                if (px[i] != curx || py[i] != cury)
+                {
+                    int a = px[i] - curx;
+                    int b = py[i] - cury;
+                    if (a*a + b*b < r2 && dx*b-a*dy >= 0)
+                    {
+                        if (nxti < 0 || a*(py[nxti] - cury) - b*(px[nxti] - curx) > 0 ||
+                            (a*(py[nxti] - cury) - b*(px[nxti] - curx) == 0 && a*a + b*b > L))
+                        {
+                            nxti = i;
+                            L = a*a + b*b;
+                        }
+                    }
+                }
+            if (nxti == -1)
+            {
+                if (r > -cury)
+                {
+                    double dL = sqrt(dx*dx + dy*dy);
+                    double angle = acos(dx / dL) + asin(-cury/r);
+                    res += r * angle;
+                    res *= 2;
+                    break;
+                }
+                else
+                {
+                    res = 2 * PI * r;
+                    break;
+                }
+            }
+            else
+            {
+                int ax = px[nxti] - curx;
+                int ay = py[nxti] - cury;
+                if (r > -cury)
+                {
+                    double bx = sqrt(r2 - cury*cury);
+                    double by = -cury;
+                    if (bx * ay - ax * by >= 0)
+                    {
+                        double angle = acos(dx / r) + asin(-cury / r);
+                        res += r * angle;
+                        res *= 2;
+                        break;
+                    }
+                }
+                double aL = sqrt(ax*ax + ay*ay);
+                double dL = sqrt(dx*dx + dy*dy);
+                double dian = dx * ax + dy * ay;
+                double angle = acos(dian / (dL * aL));
+                res += angle * r;
+                dx = px[nxti] - curx;
+                dy = py[nxti] - cury;
+                curx = px[nxti];
+                cury = py[nxti];
+                r -= aL;
+            }
+        }
+        printf("Pendulum #%d\n", ca++);
+        printf("Length of periodic orbit = %.2lf\n", res);
+        printf("\n");
+    }
+    return 0;
+}
---
+++ 2/1137/1137.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <queue>
+#include <string>
+using namespace std;
+typedef struct {
+    int state;
+    vector<string> step;
+}Step;
+
+int main()
+{
+    int r, d, s;
+    int ca = 1;
+    while (scanf("%d %d %d", &r, &d, &s) == 3)
+    {
+        if (r == 0 && d == 0 && s == 0) break;
+        int a[11][11] = { 0 };
+        char check[10240] = { 0 };
+        vector<int> sw[11];
+        for (int i = 0; i < d; i++)
+        {
+            int x, y;
+            scanf("%d %d", &x, &y);
+            a[x - 1][y - 1] = a[y - 1][x - 1] = 1;
+        }
+        for (int i = 0; i < s; i++)
+        {
+            int x, y;
+            scanf("%d %d", &x, &y);
+            sw[x - 1].push_back(y - 1);
+        }
+        for (int i = 0; i < r; i ++)
+            if (sw[i].size() > 1) sort(sw[i].begin(), sw[i].end());
+        queue<Step> qu;
+        Step cur;
+        cur.state = 10; cur.step.clear();
+        check[cur.state] = 1;
+        qu.push(cur);
+        int ok = 0;
+        while (!qu.empty())
+        {
+            cur = qu.front();
+            if (cur.state == (1 << (r - 1)) * 10 + r - 1) {
+                ok = 1; break;
+            }
+            qu.pop();
+            int room_state = cur.state / 10;
+            int cur_room = cur.state % 10;
+            //Move
+            for (int i = 0; i < r; i ++)
+                if (i != cur_room && (room_state & (1 << i)) && a[cur_room][i] && check[room_state * 10 + i] == 0)
+                {
+                    Step y;
+                    string s = "Move to room " + to_string(i + 1) + ".";
+                    y.step = cur.step;
+                    y.step.push_back(s);
+                    y.state = room_state * 10 + i;
+                    check[y.state] = 1;
+                    qu.push(y);
+                }
+            //Turn off
+            for (int i = 0; i < sw[cur_room].size(); i ++)
+                if (sw[cur_room][i] != cur_room && (room_state & (1 << sw[cur_room][i])))
+                {
+                    int new_state = (room_state - (1 << sw[cur_room][i])) * 10 + cur_room;
+                    if (check[new_state] == 0)
+                    {
+                        Step y;
+                        string s = "Switch off light in room " + to_string(sw[cur_room][i] + 1) + ".";
+                        y.step = cur.step;
+                        y.step.push_back(s);
+                        y.state = new_state;
+                        check[y.state] = 1;
+                        qu.push(y);
+                    }
+                }
+            //Turn on
+            for (int i = 0; i < sw[cur_room].size(); i++)
+                if ((room_state & (1 << sw[cur_room][i])) == 0)
+                {
+                    int new_state = (room_state + (1 << sw[cur_room][i])) * 10 + cur_room;
+                    if (check[new_state] == 0)
+                    {
+                        Step y;
+                        string s = "Switch on light in room " + to_string(sw[cur_room][i] + 1) + ".";
+                        y.step = cur.step;
+                        y.step.push_back(s);
+                        y.state = new_state;
+                        check[y.state] = 1;
+                        qu.push(y);
+                    }
+                }
+        }
+        printf("Villa #%d\n", ca++);
+        if (ok == 0) printf("The problem cannot be solved.\n");
+        else
+        {
+            printf("The problem can be solved in %d steps:\n", cur.step.size());
+            for (int i = 0; i < cur.step.size(); i++)
+            {
+                printf("- %s\n", cur.step[i].c_str());
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
